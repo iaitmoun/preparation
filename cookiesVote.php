90e9497e5773033fafae6c74209d099b6ca81548
iaitmoun <imane.ait-mouna@iteem.centralelille.fr>
2023-12-14 23:11:20 +0000

    y

--- cookiesVote.php
+++ cookiesVote.php
@@ -1,14 +1,16 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+
     if (isset($_COOKIE['exit_Cookie'])) {
-        echo 'Vous avez déjà voté! ';}
+        echo "<script> alert( 'Vous avez déjà voté!!! ');</script>";
+    }
 
     else{
         $vote= $_POST['vote'];
-        setcookie('voteCookie', $vote, time() + 86400);
+        setcookie('voteCookie', $vote, time() + 10);
         setcookie('exit_Cookie', 'Vrai' , time() + 86400);
-        echo "Merci d'avoir voter";
+        echo "<script> alert( ' Merci d avoir voter');</script>";
         }
 }
 ?>
