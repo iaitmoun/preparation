f7ef45126491cb9a617936af4c848b9827b8d277
iaitmoun <imane.ait-mouna@iteem.centralelille.fr>
2023-12-14 22:53:45 +0000

    et

--- cookiesVote.php
+++ cookiesVote.php
@@ -1,5 +1,14 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+
+    if (isset($_COOKIE['exit_Cookie'])) {
+        echo 'Vous avez déjà voté! ';}
+
+    else{
+        $vote= $_POST['vote'];
+        setcookie('voteCookie', $vote, time() + 86400);
+        setcookie('exit_Cookie', 'Vrai' , time() + 86400);
+        echo "Merci d'avoir voter";
+        }
 }
 ?>
--- index.php
+++ index.php
@@ -7,9 +7,9 @@
         <h1>Vote</h1>
 
         <form action="cookiesVote.php" method="post">
-            <input type="radio" id="demo" name="demo" value="democrates" />
+            <input type="radio" id="demo" name="vote" value="democrates" />
             <label >Democrates</label>
-            <input type="radio" id="rep" name="rep" value="rep"/>
+            <input type="radio" id="rep" name="vote" value="republicains"/>
             <label>rep</label>
             <input type="submit" value="Votez">
         </form>
