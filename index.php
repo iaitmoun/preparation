b29c40bc4bb2cb3b40659895a02841e54ff6b67a
iaitmoun <imane.ait-mouna@iteem.centralelille.fr>
2023-12-14 22:29:41 +0000

    pages

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<html>
+    <body>
+        <h1>Vote</h1>
+
+        <form action="cookiesVote.php" method="post">
+            <input type="radio" id="demo" name="demo" value="democrates" />
+            <label >Democrates</label>
+            <input type="radio" id="rep" name="rep" value="rep"/>
+            <label>rep</label>
+            <input type="submit" value="Votez">
+        </form>
+    </body>
+</html>
+
+
